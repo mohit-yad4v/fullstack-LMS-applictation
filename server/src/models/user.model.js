b4ee334f177768c12d03a22bcd1dfe47a000cc7b
mohit-yad4v <mohitkumaryadav111@gmail.com>
2025-11-16 20:43:09 +0530

    feat: implement User model with fullName, email, password, role, enrolledCourses and photoUrl fields

---
+++ server/src/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"]
+        },
+        role: {
+            type: String,
+            enum: ["instructor", "student"],
+        },
+        enrolledCourses: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Course'
+            }
+        ],
+        photoUrl: {
+            type: String,
+            default: ""
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const User = mongoose.model("User", userSchema);
